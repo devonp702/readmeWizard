30d87c9069c701482f65fdfac48268c338914b86
Blackjack2020 <69491789+devonp702@users.noreply.github.com>
2020-10-14 16:33:30 -0600

    added questions framework.

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ node index.js
 ```
 The wizard will ask you some questions, and give you a beautifully curated README.md file to add to your project.
 ![readmeWizard](https://img.shields.io/packagist/l/devonp702/readmeWizard?style=plastic)
+
+returned text:
+
+{
+  title: 'puppycat',
+  description: 'This puppycat is the best puppycat of all.',
+  installation: 'To install the puppycat please let it into your house',
+  usage: 'propper feeding and maintainance is a must',
+  license: 'MIT',
+  contributing: 'Contributor Covenant',
+  tests: 'Puppycats are not good at taking tests. they prefer to have lunch',
+  userName: 'devonp702',
+  email: 'devon.p702@gmail.com'
+}
--- index.js
+++ index.js
@@ -1,15 +1,78 @@
+/**
+ * README Wizard!
+ * run by writing `node index.js` in your console
+ */
+var fs = require("fs");
+var inquirer = require("inquirer");
 // array of questions for user
 const questions = [
-
+  {
+    type: "input",
+    name: "title",
+    message: "Enter your project title:"
+  },
+  // * Description
+  {
+    type: "input",
+    name: "description",
+    message: "A short description explaining the what, why, and how. What was your motivation? Why did you build this project? What problem does it solve?"
+  },
+  // * Installation
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running."
+  },
+  // * Usage
+  {
+    type: "input",
+    name: "usage",
+    message: "Provide instructions and examples for use"
+  },
+  // * License
+  {
+    type: "list",
+    name: "license",
+    message: "The last section of a good README is a license. This lets other developers know what they can and cannot do with your project.",
+    choices:["MIT", "GNU general public v3.0", "Apache 2.0"]
+  },
+  // * Contributing
+  {
+    type: "list",
+    name: "contributing",
+    message: "If you created something and would like other developers to contribute to it, you will want to add guidelines for how to do so.",
+    choices:["Contributor Covenant", "I want to make my own."]
+  },
+  // * Tests
+  {
+    type: "input",
+    name: "tests",
+    message: "Go the extra mile and write tests for your application. Then provide examples on how to run them."
+  },
+  // * Questions
+  {
+    type: "input",
+    name: "userName",
+    message: "Please enter your github user name:"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please enter your email address:"
+  }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data);
 }
 
 // function to initialize program
 function init() {
-
+  inquirer.prompt(questions)
+  .then(answers => console.log(answers))
+  //format the data
+  // writeToFile("README.md", readMe);
 }
 
 // function call to initialize program
